95ed9c21e3214f769bc52caa3bc0ef8166937793
Anand Devarajan <anand.devarajan@multicorewareinc.com>
2022-06-13 17:44:22 +0530

    encrypted pwd

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -2,12 +2,30 @@ import mongoose from "mongoose"
 import bcrypt from 'bcrypt'
 import config from 'config'
 
+export interface UserDocument extends mongoose.Document {
+    email: string
+    name: string
+    password: string
+    createdAt: Date
+    updateAt: Date
+}
+
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     password: { type: String, required: true }
 }, {
     timestamps: true
+})
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified('password')) {
+        return next()
+    }
+    const salt = await bcrypt.genSalt(config.get<number>('saltWorkFactor'))
+    const hash = await bcrypt.hashSync(this.password, salt);
+    this.password = hash
+    return next()
 })
 
 const User = mongoose.model("User", userSchema)
