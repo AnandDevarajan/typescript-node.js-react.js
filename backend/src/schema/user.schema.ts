1b7758ec88cb69f2deaaf9f209e654d8654e3c5d
Anand Devarajan <anand.devarajan@multicorewareinc.com>
2022-06-13 21:28:58 +0530

    validation for User schema

---
+++ backend/src/schema/user.schema.ts
@@ -0,0 +1,23 @@
+import { object, string } from "zod"
+export const createUserSchema = object({
+    body: object({
+        name: string({
+            required_error: "Name is required"
+        }),
+        email: string({
+            required_error: "Email is required"
+        }).email('Not a valid email'),
+        password: string({
+            required_error: "Password is required"
+        }).min(6, "Password too short-should be 6 chars minumum"),
+        passwordConfirmation: string({
+            required_error: "Confrim password is required"
+        })
+    }).refine((data) => data.password === data.passwordConfirmation, {
+        message: "Passwords do not match",
+        path: ["passwordConfirmation"] //path of error
+    })
+})
+
+
+
